5ff1070e9a9dfeab788a6eaecf7ae4ca88702bd3
Pnoy <pnoy2012@hotmail.com>
2013-07-03 19:57:29 -0400

    modified:   src/World.java 	deleted:    src/charXMLs/appleJack 	deleted:    src/charXMLs/flutterShy 	deleted:    src/charXMLs/pinkiePie 	deleted:    src/charXMLs/player 	deleted:    src/charXMLs/rainbowDash 	deleted:    src/charXMLs/rarity 	deleted:    src/charXMLs/spike 	deleted:    src/charXMLs/twilightSparkle 	deleted:    src/font/DoktorTerror.ttf 	deleted:    src/font/Dutch & Harley alt.ttf 	deleted:    src/font/Dutch & Harley.ttf 	deleted:    src/font/FPENSTRIAL.ttf 	deleted:    src/font/Homev2.ttf 	deleted:    src/font/Homev2c.ttf 	deleted:    src/font/Homev2e.ttf 	deleted:    src/font/Homev2ei.ttf 	deleted:    src/font/Homev2i.ttf 	deleted:    src/font/Homev2s.ttf 	deleted:    src/font/INVASION2000.TTF 	deleted:    src/font/JOURNAL.TTF 	deleted:    src/font/Zack and Sarah.ttf 	deleted:    src/font/data-latin.ttf 	deleted:    src/font/data-unifon.ttf 	deleted:    src/font/destructobeambb_reg.ttf 	deleted:    src/font/electron 8f.ttf 	deleted:    src/font/ilits.ttf 	deleted:    src/font/jd_led5.ttf 	deleted:    src/font/look sir, droids.ttf 	deleted:    src/font/triple_dot_digital-7.ttf 	deleted:    src/invXMLs/Equestria_w 	deleted:    src/invXMLs/_relations 	deleted:    src/invXMLs/_w 	deleted:    src/invXMLs/attributesList 	deleted:    src/invXMLs/characters 	deleted:    src/invXMLs/commands 	deleted:    src/invXMLs/exceptions 	deleted:    src/invXMLs/objects 	deleted:    src/invXMLs/pronouns 	deleted:    src/invXMLs/toTarLinking 	deleted:    src/invXMLs/useLinking 	deleted:    src/titleGen/titleList 	deleted:    src/xMLGuides/XML Guide 	deleted:    src/xMLGuides/attributesList 	deleted:    src/xMLGuides/outputMap 	src/defaults/

--- src/World.java
+++ src/World.java
@@ -8,26 +8,34 @@ import java.util.StringTokenizer;
 public class World {
 	File worldFile;
 	File worldFolder;
-	public void worldInit() {
+	public void worldInit() throws IOException {
 		worldFolder = new File("save/worldXMLs");
-		// scan folder for world files
-		// Static "Equestria" variable for now. TO CHANGE
+		//If specified world is found, load world data from it
+		//If false, either create new world or try loading different one depending on
+		//if any other worlds are available.
 		if (isWorldFound(worldFolder, "Equestria")) {
-			//TODO: What to do if world files found.
-			loadPlayer(worldFile);
+			//TODO FINISH: What to do if world files found.
+			//TODO: Include dynamic character parameter for use by NPCs, not just player
+			loadCharacter(worldFile, "player");
 		} else {
+			//Call method to generate new world in save/worldXMLs creating a worldName_w file
 			createNewWorld();
 		}
 	}
+	//Returns true if the specified world is found inside the save/worldXMLs folder
+	//returns false if it is not found
 	public boolean isWorldFound(File worldFolder, String worldName) {
 		boolean worldFound = false;
 		try {
+			//if there are files in the worldXMLs directory, try to find specified world
+			//otherwise, false
 			if (worldFolder.listFiles().length != 0) {
 				worldFile = new File(worldFolder
 						+ "/"
 						+ worldName
 						+ "_w");
 				try {
+					//method returns true if worldFile is found, this determines whether file exists or not.
 					if(worldFile.exists()){
 						worldFound = true;
 					}
@@ -36,33 +44,48 @@ public class World {
 		} catch (Exception noWorldsFound) {}
 		return worldFound;
 	}
-	//to be done
+	//TODO: generate a new world based on using worldName parameter for the name of the New world.
 	public void createNewWorld() {
+		//
 		BufferedReader bis = new BufferedReader(new InputStreamReader(
 				getClass().getResourceAsStream("invXMLs/_w")));
 	}
-	public void loadPlayer(File worldDir) {
-		//load information from world file to game.
-		File playerFile = new File("save/charXMLs/player");
+	//This method will be used to call functions and methods that are relevant to loading
+	//information that refers to the player.
+	//TODO: Availability of objects and characters to the doer of a command based on locations
+	//TODO: Access to other locations from a location
+	//TODO: Location specific attributes, traits and other information that can be accessed
+	public void loadCharacter(File worldDir, String charDir) throws IOException {
+		//Character xml object: the doer.
+		File playerFile = new File("save/charXMLs/" + charDir);
 		try {
 			if(playerFile.exists()){
+				//debug information
+				//TODO: With location information, find relevant information from worldName_w file such as:
+				//objects in location, characters in location, description of loc, traits of loc, name of loc, ect.
 				System.out.println(findLocation(playerFile));
 			}else{
-				//If player needs to be created
+				//TODO: If player file is missing or needs to be created.
 			}
-		} catch (Exception e) {
+		} catch (FileNotFoundException fnfe) {
+			//TODO: If playerfile is missing or needs to be created.
 		}
 	}
+	//Find the locations variable information for the character that is requested
+	//Returns a string with all the variable's names seperated by underscores.
+	//variables: worldName, regionName, areaName, localName.
 	public String findLocation(File playerFile) throws IOException{
 		BufferedReader bis;
 		String readIn = null;
 		StringTokenizer st;
 		int counter = 0;
 		String locString = "";
+		//we already checked if playerfile exists, since it does, we will need to fetch <placement> from it
 		bis = new BufferedReader(new InputStreamReader(
 				new FileInputStream(playerFile)));
 		do{
 			readIn = bis.readLine();
+			//if a line contains either of the four variable names, then we tokenize the variable data and read it in.
 			if(readIn.contains("worldName")
 					|| readIn.contains("regionName")
 					|| readIn.contains("areaName")
@@ -70,7 +93,8 @@ public class World {
 				st = new StringTokenizer(readIn);
 				st.nextToken("\""); //Tokenizer scrap
 				locString += (st.nextToken("\"")
-						+ "_"); //grab xml data
+						+ "_"); //grabs xml data, appends an underscore after variable data to be tokenized
+				//from the string it is being appended to later.
 				counter++;
 				if(counter >= 4){
 					break;
@@ -78,6 +102,7 @@ public class World {
 			}
 		}while(readIn.equals(null) == false);
 		bis.close();
+		//returns a string containing all the variable information.
 		return locString;
 	}
 }
