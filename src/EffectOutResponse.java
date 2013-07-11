fb4a1973e4160be12a251fd125f3f2d23881f8e5
AnonPower <flow_ous30@hotmail.com>
2013-07-11 12:32:46 -0700

    Update EffectOutResponse.java
    Added two extra functions, will look for a dictionary XML or create one soon to define the emotions on the words.

--- src/EffectOutResponse.java
+++ src/EffectOutResponse.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
-/*v0.5
+/*
  * EffectOutResponse class
  * 	Responsibility: Carry responses from characters from the XML
  */
+import java.util.ArrayList;
 public class EffectOutResponse {
-	XMLParse xP = new XMLParse();
+	XMLParse 								xP 			= new XMLParse();
+	Attributes 							attrib 	= new Attributes();
+	static	StringBuilder		sB			= new StringBuilder();
+	static private 	String 	expt;
 
 	/*
 	 * Response Initiation
-	 * #String
+	 * 	#String
 	 * 		@param 'doer' 			player or character taking an action
 	 * 		@param 'target'			player or character being taken acting upon
 	 * 		@param 'targetAttribute'target's XML attribute
@@ -19,31 +23,29 @@ public class EffectOutResponse {
 	 * 		@param 'tIntEAdd'		Apply addition
 	 * 		@param 'tIntESub'		Apply substitution
 	 */
-	public String responseInit(	String 	doer,
-												String 	target,
-												String 	targetAttribute,
-												String 	tStrE,
-												String 	tBooE,
-												String	tIntEEqu,
-												int 		tIntEAdd,
-												int 		tIntESub)
+	public 	String responseInit(String 	doer,
+							String 	target,
+							String 	targetAttribute,
+							String 	tStrE,
+							String 	tBooE,
+							String	tIntEEqu,
+							int 		tIntEAdd,
+							int 		tIntESub)
 	throws IOException, InterruptedException {
 		xP.xMLArraysWipe();
 
-		xP.xMLParser("defaults/MLTG/invXMLs/attributesList");
+		xP.xMLParser("/invXMLs/attributesList");
 
-		String responseOut = "";
-
-		ValueWork vW 	= new ValueWork();
-
-		Relations rel = new Relations();
+		String 		responseOut = "";
+		ValueWork 	vW 			= new ValueWork();
+		Relations 	rel 		= new Relations();
 		
 		int change = vW.determineRelationChange(targetAttribute,
-																			tStrE,
-																			tBooE,
-																			tIntEEqu,
-																			tIntEAdd, 
-																			tIntESub);
+												tStrE,
+												tBooE,
+												tIntEEqu,
+												tIntEAdd, 
+												tIntESub);
 		if (change == 0) {
 			//If nothing is changed, give out a neutral response
 			responseOut = neutralResponse(doer, target);
@@ -51,24 +53,24 @@ public class EffectOutResponse {
 			if (change > 0) {
 				//If there is a positive change, Give a good response!
 				responseOut = goodResponse(	doer,
-															target,
-															targetAttribute,
-															tStrE,
-															tBooE,
-															tIntEEqu,
-															tIntEAdd,
-															tIntESub);
+											target,
+											targetAttribute,
+											tStrE,
+											tBooE,
+											tIntEEqu,
+											tIntEAdd,
+											tIntESub);
 			} else {
 				if (change < 0) {	
 					//If there is a negative change, Give a bad response!
 					responseOut = badResponse(	doer,
-																target,
-																targetAttribute,
-																tStrE,
-																tBooE,
-																tIntEEqu, 
-																tIntEAdd,
-																tIntESub);
+												target,
+												targetAttribute,
+												tStrE,
+												tBooE,
+												tIntEEqu, 
+												tIntEAdd,
+												tIntESub);
 				}
 			}
 		}
@@ -103,17 +105,18 @@ public class EffectOutResponse {
 	 * 									by player or NPC
 	 */
 	public String goodResponse(	String 	doer,
-												String 	target,
-												String 	targetAttribute,
-												String 	tStrE,
-												String 	tBooE,
-												String 	tIntEEqu,
-												int 		tIntEAdd,
-												int 		tIntESub )
+							String 	target,
+							String 	targetAttribute,
+							String 	tStrE,
+							String 	tBooE,
+							String 	tIntEEqu,
+							int 		tIntEAdd,
+							int 		tIntESub )
 	throws IOException, InterruptedException {
+		float calc;
 		String response;
-
 		if (doer.equals(target)) {
+			//System.out.print(attrib.findCharacterGender(target));
 			response = (target + " makes themself happy.");
 		} else {
 			if (doer.equals("player")) {
@@ -143,13 +146,13 @@ public class EffectOutResponse {
 	 * 									by player or NPC
 	 */
 	public String badResponse(	String 	doer,
-												String 	target,
-												String 	targetAttribute,
-												String 	tStrE,
-												String 	tBooE,
-												String 	tIntEEqu,
-												int 		tIntEAdd,
-												int 		tIntESub ) 
+								String 	target,
+								String 	targetAttribute,
+								String 	tStrE,
+								String 	tBooE,
+								String 	tIntEEqu,
+								int 		tIntEAdd,
+								int 		tIntESub ) 
 	throws IOException, InterruptedException {
 		String response;
 		if (doer.equals(target)) {
@@ -190,13 +193,143 @@ public class EffectOutResponse {
 
 		return response;
 	}
+	/*
+	 * Word decisions
+	 * 	#String
+	 * 		@param	'input'			OPTIONAL if it's not a question ask something
+	 * 		@param	'question'		IF a question, answer it with a value
+	 * 		@param	'effectiveWord'	A single effective word that has a higher value in
+	 * 								modifying the bad-good for the context/sentence mood
+	 * 		@param	'context'		Arrayed, separated words by every single space.
+	 */
+	public void wordChoice(String input,String question,String effectiveWord,String context){
+		//simplify
+		String 	in 			= input;
+		String 	ques 		= question;
+		String 	eW			= effectiveWord;
+		String 	cox 		= context;
+		
+		boolean isQ 		= false;
+		float 	relation 	= (float) 0.0;	//bad-good type of determination
+		
+		if(relation>50.0){
+			if(relation>75.0){
+				relation = (float) ((float) relation / (float)2.0);
+			}
+		}else if(relation<50.0){
+			if(relation<25.0){
+				relation += (float) 0.10;
+			}
+		}else{
+			relation = (float) 0.0;
+		}
+		
+		if(question != null){
+			input.toLowerCase();		//fix typos
+			if(	input.contains("what")
+			||	input.contains("who")
+			||	input.contains("why")
+			||	input.contains("when")
+			||	input.contains("which")
+			||	input.contains("how")){
+				isQ = true;
+			}else{
+				isQ = false;
+			}
+			//if there's no question, ask something
+				//If does not have
+					//WHO
+					//WHAT
+					//WHY
+					//WHEN
+					//WHICH
+					//HOW
+			//it is not a question. Exceptions may varie in a library of question-like
+			//sarting words, or if ends with a "?"
+		}else{
+			//if it's a question, answer with something related.
+			
+			//determine if the question is good/bad
+			//Use from the library
+					//WHO
+					//WHAT
+					//WHY
+					//WHEN
+					//WHICH
+					//HOW
+			//Relate to what will happen after getting these, and generate a sentence
+			//related to the context of these words.
+				//If some other form of question, use a different method of answering.
+			//NOTICE: If ends with a "?" it is a question. Exceptions may varie.
+		}
+	}
+	/*v0.1
+	 * Mood detection of words
+	 * 	#String
+	 * 		@param 'context'	Takes in the paragraph and determines how the
+	 * 							mood is going good or bad on a scale of bad-good.
+	 * 	#Integer
+	 * 		@param 'length'		The length of the paragraph.
+	 * 		
+	 */
+	public void moodDetection(String context, int length){
+		length = context.length();
+		//context = toWords(context);	
+	}
+	////////////////////////////////////////////////////////////////
+	//////These functions may be used in other things as well.//////
+	////////////////////////////////////////////////////////////////
+	/*
+	 *	EXPLODE
+	 *	Puts a string into arrays. Name inspiration from PHP
+	 *		@param	String	str				Input string
+	 *		@param	String	exception	Input exception
+	 *
+	 *		@return	String[]	split		The String splitted into an array
+	 */
+	public static String[] explode(String str,String exception){
+		expt = exception;
+		String[] split = str.split(exception);
+		for(int i =0;i< split.length;i++){
+			sB.append(split[i]);
+			if(i!=split.length-1){
+				sB.append(" ");
+			}
+		}
+		return split;
+	}
+
+	/*
+	 *	IMPLODE
+	 *	Puts the array back into a string. Name inspiration from PHP
+	 *		@param	String[]	arr				Input array
+	 *		@param	String		exception	Exception from stored gobal var by explode()
+	 *
+	 *		@return String		The array put together with the exception
+	 */
+	public static String implode(String[] arr,String exception){
+		//Get String from global var. If not safe, please make a new method!
+		//Functions would be eh, but this is OK for now.
+		exception = expt;
+		if((exception==null)
+		|| (exception=="")){
+			//Set to [space] spacing if there is nothing.
+			exception=" ";
+		}
+		String appendedString="";
+		for(int i=0;i<arr.length;++i){
+			appendedString += arr[i] + exception;
+		}
+		return appendedString;
+		
+	}
 	
 	//serious 		response	--	new
-	//angry 			response	--	bad or good or neutral
-	//sad					response	--	bad or good or neutral
-	//happy				response	--	bad or good or neutral
-	//proud				response	--	bad or good or neutral
-	//naive				response	--	bad or good or neutral
+	//angry 		response	--	bad or good or neutral
+	//sad			response	--	bad or good or neutral
+	//happy			response	--	bad or good or neutral
+	//proud			response	--	bad or good or neutral
+	//naive			response	--	bad or good or neutral
 	//determined	response	--
 	
 }
